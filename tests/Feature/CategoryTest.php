77e18fa6a21e17ddfc60666e3f7d467c1255d83b
azdyf <azdy.fm@google.com>
2025-06-28 11:29:27 +0700

    Update

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -61,4 +61,23 @@ class CategoryTest extends TestCase
         self::assertTrue($result);
     }
 
+    public function testSelect()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $category = new Category();
+            $category->id = 'ID ' . $i;
+            $category->name = 'Name ' . $i;
+            $category->save();
+        }
+
+        $categories = Category::whereNull('description')->get();
+        self::assertEquals(5, $categories->count());
+        $categories->each(function (Category $category) {
+            self::assertNull($category->description);
+
+            $category->description = "Updated";
+            $category->update();
+        });
+    }
+
 }
