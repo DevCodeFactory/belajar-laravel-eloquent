afefd1636adc300942ae9ef3fcb35178ae1697e5
azdyf <azdy.fm@google.com>
2025-06-28 20:48:16 +0700

    Aggregating Relations

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -268,4 +268,16 @@ class CategoryTest extends TestCase
         self::assertEquals('2', $products[0]->id);
     }
 
+    public function testAggregatingRelations()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $category = Category::find('FOOD');
+        $total = $category->products()->count();
+        self::assertEquals(2, $total);
+
+        $total = $category->products()->where('price', '=', 200)->count();
+        self::assertEquals(1, $total);
+    }
+
 }
