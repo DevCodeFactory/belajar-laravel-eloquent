f87841a946c5fbcf8b003519f8a2ba2ca7f02af6
azdyf <azdy.fm@google.com>
2025-06-28 08:57:03 +0700

    Find

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -12,6 +12,10 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $category = new Category();
+        $category->id = 'FOOD';
+        $category->name = 'Food';
+        $category->description = 'Food Category';
+        $category->save();
     }
 }
--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use Database\Seeders\CategorySeeder;
 use Tests\TestCase;
+use function PHPUnit\Framework\assertEquals;
 
 class CategoryTest extends TestCase
 {
@@ -34,6 +36,18 @@ class CategoryTest extends TestCase
 //        $total = Category::query()->count();
         $total = Category::count();
         self::assertEquals(10, $total);
+    }
+
+    public function testFind()
+    {
+        $this->seed(CategorySeeder::class);
+
+        $category = Category::query()->find('FOOD');
+
+        self::assertNotNull($category);
+        assertEquals('FOOD', $category->id);
+        assertEquals('Food', $category->name);
+        assertEquals('Food Category', $category->description);
     }
 
 }
