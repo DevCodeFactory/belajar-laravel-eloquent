3097afae9b124f50c8acb63dd7bb9f053bd3da41
azdyf <azdy.fm@google.com>
2025-06-28 11:34:44 +0700

    Delete

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -100,4 +100,17 @@ class CategoryTest extends TestCase
         self::assertEquals(10, $total);
     }
 
+    public function testDelete()
+    {
+        $this->seed(CategorySeeder::class);
+
+        $category = Category::find('FOOD');
+        $result = $category->delete();
+
+        self::assertTrue($result);
+
+        $total = Category::count();
+        self::assertEquals(0, $total);
+    }
+
 }
