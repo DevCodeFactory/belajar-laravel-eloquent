e28b7d9173e591dc0eb5986728212f798f7b26fa
azdyf <azdy.fm@google.com>
2025-06-28 08:27:15 +0700

    Insert Data And Add Log Query

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    public function testInsert()
+    {
+        $category = new Category();
+        $category->id = 'GADGET';
+        $category->name = 'Gadget';
+        $result = $category->save();
+
+        self::assertTrue($result);
+    }
+
+}
