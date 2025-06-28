795a7bcca2a7aabc99f7eb24b1c5f00e6fda8dfd
azdyf <azdy.fm@google.com>
2025-06-28 16:53:17 +0700

    Has One Through

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Customer;
 use App\Models\Wallet;
 use Database\Seeders\CustomerSeeder;
+use Database\Seeders\VirtualAccountSeeder;
 use Database\Seeders\WalletSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -39,6 +40,18 @@ class CustomerTest extends TestCase
         $customer->wallet()->save($wallet);
 
         self::assertNotNull($wallet->customer_id);
+    }
+
+    public function testHasOneThrough()
+    {
+        $this->seed([CustomerSeeder::class, WalletSeeder::class, VirtualAccountSeeder::class]);
+
+        $customer = Customer::find('FAHMI');
+        self::assertNotNull($customer);
+
+        $virtualAccount = $customer->virtualAccount;
+        self::assertNotNull($virtualAccount);
+        self::assertEquals('BCA', $virtualAccount->bank);
     }
 
 }
